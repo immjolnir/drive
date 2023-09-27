27e4502e84f8624effe1c809f2ad419832bcb0e8
zhishan <zhishanlee@gmail.com>
2023-09-27 15:32:28 +0800

    basis/list only be used when there's random access

--- modules/basis/list.cpp
+++ modules/basis/list.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <algorithm>  // std::find
 #include <list>
 
 /**
@@ -11,15 +12,22 @@
  * Adding, removing and moving the elements within the list or across several lists does not invalidate the iterators or
  * references. An iterator is invalidated only when the corresponding element is deleted.
  *
-  back(tail)       front(head)
-    |                   |
-    v                   v
-    +---+---+---+---+---+
-    |   |   |   |   |   |
-    +---+---+---+---+---+
-push_back             push_front
-emplace_back          emplace_front
-pop_back              pop_front
+ *
+ * begin/end definition: https://en.cppreference.com/w/cpp/container/list/end
+ * begin: Returns an iterator to the first element of the list.
+ *   If the list is empty, the returned iterator will be equal to end().
+ * end: Returns an iterator to the element following the last element of the list.
+ *
+       front/head               back/tail
+           V                        V
+        .-----+---+---+---+---+---+---+---
+        |begin|   |   |   |   |   |   | end(past-the-last-element)
+        '-----+---+---+---+---+---+---+---
+          ^                         ^
+    push_front                  push_back
+    emplace_front               emplace_back
+    pop_front                   pop_back
+
 
  * When adding a new element with push_back()/emplace_back() and deleting the old element with pop_front(),
  * the front() is the oldest.
@@ -28,7 +36,6 @@ pop_back              pop_front
  *
  * emplace_back: constructs an element in-place at the end
  * push_back: adds an element to the end
- * pop_front: removes the first element
  *
  * front: access the frist element
  * back: access the last element
@@ -38,11 +45,17 @@ TEST(list, treat_it_as_circlar_buffer) {
     std::list<int> my_list = {1, 2, 3, 4};
     EXPECT_EQ(1, my_list.front());  // the first(oldest) element is the head value
     EXPECT_EQ(4, my_list.back());   // the last(newest) element is the tail value
+    EXPECT_EQ(my_list.front(), *my_list.begin());
+    EXPECT_EQ(my_list.back(), *(std::prev(my_list.end())));
 
     my_list.emplace_front(6);
     my_list.push_back(5);
     EXPECT_EQ(6, my_list.front());
     EXPECT_EQ(5, my_list.back());
+
+    // Print list.
+    std::for_each(my_list.begin(), my_list.end(), [](const int n) { std::cout << n << ' '; });
+    std::cout << '\n';
 }
 
 TEST(list, pop_front_pop_back) {
@@ -114,4 +127,72 @@ TEST(list, deleting) {
     }
 }
 
-// Is there a better way to find the exact element and remove the oldest?
+// Is there a better way than above to find the exact element and remove the oldest?
+TEST(list, deleting_batch) {
+    // erase
+    // Erases the specified elements from the container.
+    // * Removes the element in pos
+    // iterator erase( iterator pos );
+    // iterator erase( const_iterator pos );
+    //
+    // * Removes the elements in the range [first, last).
+    // iterator erase( iterator first, iterator last );
+    // iterator erase( const_iterator first, const_iterator last );
+    std::list<int> my_list = {1, 2, 3, 4};
+    /*
+            .---+---+---+---+---+---+---
+            | 1 | 2 | 3 | 4 |   |   |
+            '---+---+---+---+---+---+---
+                      ^
+                    found
+            |<-  ->| deleting ranges
+    */
+    auto found = std::find(my_list.begin(), my_list.end(), 3);
+    if (found != my_list.end()) {
+        my_list.erase(my_list.begin(), found);
+    }
+    EXPECT_EQ(2, my_list.size());
+    EXPECT_EQ(3, my_list.front());
+    EXPECT_EQ(4, my_list.back());
+}
+
+// Actually, if there's no random RW, we can use vector/array.
+// The underlying of the list is double-linked list.
+//
+//  > std::list is a container that supports constant time insertion and removal of elements from anywhere in the
+//  > container. Fast random access is not supported. It is usually implemented as a doubly-linked list.
+
+// Radom inserting
+TEST(list, radom_insert) {
+    std::list<int> my_list = {1, 2, 3, 4};
+    EXPECT_EQ(4, my_list.size());
+    EXPECT_EQ(1, my_list.front());
+    EXPECT_EQ(4, my_list.back());
+
+    // Insert an integer before 3 by searching
+    auto it = std::find(my_list.begin(), my_list.end(), 3);
+    EXPECT_TRUE(it != my_list.end());
+    // Inserts elements at the specified location in the container.
+    my_list.insert(it, 42);
+    EXPECT_EQ(5, my_list.size());
+    EXPECT_EQ(1, my_list.front());
+    EXPECT_EQ(4, my_list.back());
+
+    {
+        // Remove the element: 42
+        auto found = std::find(my_list.begin(), my_list.end(), 42);
+        EXPECT_TRUE(found != my_list.end());
+        my_list.erase(found);          // random deleting
+        EXPECT_EQ(4, my_list.size());  // back to original size
+        EXPECT_EQ(1, my_list.front());
+        EXPECT_EQ(4, my_list.back());
+    }
+}
+
+TEST(list, clear) {
+    std::list<int> my_list = {1, 2, 3, 4};
+    EXPECT_EQ(4, my_list.size());
+    my_list.clear();
+    EXPECT_EQ(0, my_list.size());
+    EXPECT_TRUE(my_list.empty());
+}
