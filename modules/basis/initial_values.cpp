bbfbc308aa9f0c5b9710aea133845a28d14c05e6
zhishan <zhishanlee@gmail.com>
2023-10-17 16:34:30 +0800

    Add initial_values.cpp
    remove optional_impl.cpp

---
+++ modules/basis/initial_values.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+// What are the initial values, if any, of each of the following variables?
+// C++ Primer, Fifth Edition P75
+//
+std::string global_str;
+int global_int;
+
+int main() {
+    int local_int; 
+    std::string local_str;
+
+    std::cout << global_str << std::endl;
+    std::cout << global_int << std::endl;
+
+    std::cout << local_int << std::endl; // undefined value.
+    std::cout << local_str << std::endl;
+
+}
+
+/*
+ *
+$ ./a.out 
+
+0
+32714
+
+
+ */
