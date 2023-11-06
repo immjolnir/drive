2c8181ee7ce3fce2d91a32f567ec57a847606ad7
lizhishan <zhishanlee@gmail.com>
2023-11-06 12:05:47 +0800

    Add forward_iterator

--- modules/basis/iterator/iterator_category.cpp
+++ modules/basis/iterator/iterator_category.cpp
@@ -27,10 +27,14 @@ output_iterator_tag
  * - Input Iterator
  * - Forward Iterator
  * - Bidirectional Iterator
+ *   - Increment and decrement are inverses of each other:
+ *      - If a is incrementable and bool(a == b), then bool(--(++a) == b).
+ *      - If a is decrementable and bool(a == b), then bool(++(--a) == b).
  * - Random Access Iterator
  *
  */
 
+#include <forward_list>  // since c++11
 #include <list>
 #include <type_traits>
 #include <vector>
@@ -38,6 +42,193 @@ output_iterator_tag
 #include <iterator>
 
 #include <algorithm>  // iter_swap
+
+// Part 1: familar with the container's iterator
+/*
+// error: too many template-parameter-lists
+template <typename ExpectedType>
+template <typename Iter>
+bool check_container_iterator(Iter iter) {
+    return std::is_same_v<std::iterator_traits<Iter::iterator>::iterator_category, ExpectedType>;
+}
+*/
+
+template <typename ExpectedType, typename Iter>
+bool check_container_iterator(Iter iter) {
+    typedef typename std::iterator_traits<Iter>::iterator_category Category;
+    return std::is_same_v<Category, ExpectedType>;
+}
+
+namespace google::protobuf::internal {
+// src/google/protobuf/repeated_field.h
+
+// 注释掉本类
+// template <typename Iter>
+// inline int CalculateReserve(Iter begin, Iter end, std::random_access_iterator_tag) {
+//     return -2;
+// }
+
+template <typename Iter>
+inline int CalculateReserve(Iter begin, Iter end, std::forward_iterator_tag) {
+    return static_cast<int>(std::distance(begin, end));
+}
+
+template <typename Iter>
+inline int CalculateReserve(Iter /*begin*/, Iter /*end*/, std::input_iterator_tag /*unused*/) {
+    return -1;
+}
+
+template <typename Iter>
+inline int CalculateReserve(Iter begin, Iter end) {
+    typedef typename std::iterator_traits<Iter>::iterator_category Category;
+    return CalculateReserve(begin, end, Category());
+}
+}  // namespace google::protobuf::internal
+
+TEST(iterator_category, vector_iterator_type) {
+    // vector is random_access_iterator_tag not bidirectional_iterator_tag
+    std::vector<int> v{1, 2};
+    //  In instantiation of 'bool check_container_iterator(Iter) [with ExpectedType = std::random_access_iterator_tag;
+    //  Iter = __gnu_cxx::__normal_iterator<int*, std::vector<int> >]':
+    EXPECT_FALSE(check_container_iterator<std::forward_iterator_tag>(v.begin()));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(v.begin()));
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(v.begin()));
+    EXPECT_GT(google::protobuf::internal::CalculateReserve(v.begin(), v.end()),
+              -1);  // 模板匹配的时候，base class 也能匹配成功。即母板匹配逻辑时：相同类型（本类） > 可转换类型（父类）
+
+    // cbegin:
+    //    Returns a read-only (constant) iterator that points to the first element in the %vector.
+    //    Iteration is done in ordinary element order.
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(v.cbegin()));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(v.cbegin()));
+
+    // rbegin:
+    //   Returns a read/write reverse iterator that points to the last element in the %vector.
+    //   Iteration is done in reverse element order.
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(v.rbegin()));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(v.rbegin()));
+
+    // rcbegin:
+    //
+}
+
+TEST(iterator_category, raw_array_iterator_type) {
+    // raw array
+    int v[]{1, 2, 3, 4, 5};
+    // std::begin(data) error: 'decltype' evaluates to 'int*', which is not a class or enumeration type
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(std::begin(v)));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(std::begin(v)));
+    EXPECT_GT(google::protobuf::internal::CalculateReserve(std::begin(v), std::end(v)), -1);
+
+    // cbegin:
+    //      Returns a read-only (constant) iterator that points to the first element in the %list.
+    //      Iteration is done in ordinary element order.
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(std::cbegin(v)));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(std::cbegin(v)));
+
+    // rbegin:
+    //       Return a reverse iterator pointing to the last element of the array.
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(std::rbegin(v)));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(std::rbegin(v)));
+
+    // Use std::array instead the raw array in c++.
+}
+
+TEST(iterator_category, std_array_iterator_type) {
+    // Use std::array instead the raw array in c++.
+    std::array<int, 5> v{1, 2, 3, 4, 5};
+
+    // std::begin(data) error: 'decltype' evaluates to 'int*', which is not a class or enumeration type
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(v.begin()));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(v.begin()));
+    EXPECT_GT(google::protobuf::internal::CalculateReserve(v.begin(), v.end()), -1);
+
+    // cbegin:
+    //      Returns a read-only (constant) iterator that points to the first element in the %list.
+    //      Iteration is done in ordinary element order.
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(v.cbegin()));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(v.cbegin()));
+
+    // rbegin:
+    //       Return a reverse iterator pointing to the last element of the array.
+    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(v.rbegin()));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(v.rbegin()));
+    // rcbegin:
+    //
+}
+
+// List is bidirectional_iterator instead of random_access_iterator
+/**
+ *  /usr/include/c++/12/bits/stl_list.h
+   *  @brief A list::iterator.
+   *
+   *  All the functions are op overloads.
+  template<typename _Tp>
+    struct _List_iterator
+    {
+      typedef _List_iterator<_Tp>		_Self;
+      typedef _List_node<_Tp>			_Node;
+
+      typedef ptrdiff_t				difference_type;
+      typedef std::bidirectional_iterator_tag	iterator_category; // hardcoded iterator tag.
+      typedef _Tp				value_type;
+      typedef _Tp*				pointer;
+      typedef _Tp&				reference;
+
+ */
+TEST(iterator_category, list_iterator_type) {
+    std::list<int> v{1, 2};
+    EXPECT_FALSE(check_container_iterator<std::random_access_iterator_tag>(v.begin()));
+    EXPECT_TRUE(check_container_iterator<std::bidirectional_iterator_tag>(v.begin()));
+    EXPECT_GT(google::protobuf::internal::CalculateReserve(v.begin(), v.end()), -1);
+
+    // cbegin:
+    //      Returns a read-only (constant) iterator that points to the first element in the %list.
+    //      Iteration is done in ordinary element order.
+    EXPECT_FALSE(check_container_iterator<std::random_access_iterator_tag>(v.cbegin()));
+    EXPECT_TRUE(check_container_iterator<std::bidirectional_iterator_tag>(v.cbegin()));
+
+    // rbegin:
+    //      Returns a read/write reverse iterator that points to the last element in the %list.
+    //      Iteration is done in reverse element order.
+    EXPECT_FALSE(check_container_iterator<std::random_access_iterator_tag>(v.rbegin()));
+    EXPECT_TRUE(check_container_iterator<std::bidirectional_iterator_tag>(v.rbegin()));
+}
+
+// std::forward_list is forward_iterator only.
+/**
+ * /usr/include/c++/12/bits/forward_list.h
+ template<typename _Tp>
+    struct _Fwd_list_iterator
+    {
+      typedef _Fwd_list_iterator<_Tp>		_Self;
+      typedef _Fwd_list_node<_Tp>		_Node;
+
+      typedef _Tp				value_type;
+      typedef _Tp*				pointer;
+      typedef _Tp&				reference;
+      typedef ptrdiff_t				difference_type;
+      typedef std::forward_iterator_tag		iterator_category; // hardcoded iterator tag.
+*/
+TEST(iterator_category, forward_list_iterator_type) {
+    std::forward_list<int> v{1, 2};
+    EXPECT_FALSE(check_container_iterator<std::random_access_iterator_tag>(v.begin()));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(v.begin()));
+    EXPECT_TRUE(check_container_iterator<std::forward_iterator_tag>(v.begin()));
+    EXPECT_GT(google::protobuf::internal::CalculateReserve(v.begin(), v.end()), -1);
+
+    // cbegin:
+    //      Returns a read-only (constant) iterator that points to the first element in the %list.
+    //      Iteration is done in ordinary element order.
+    EXPECT_FALSE(check_container_iterator<std::random_access_iterator_tag>(v.cbegin()));
+    EXPECT_FALSE(check_container_iterator<std::bidirectional_iterator_tag>(v.cbegin()));
+    EXPECT_TRUE(check_container_iterator<std::forward_iterator_tag>(v.cbegin()));
+
+    // rbegin:
+    //      forward_list doesn't have it.
+}
+
+// Part 2: Practice
 
 /**
  * Example
@@ -100,53 +291,6 @@ constexpr  // since C++20
         while (first != last && first != --last) std::iter_swap(first++, last);
 }
 }  // namespace std2
-
-/*
-// error: too many template-parameter-lists
-template <typename ExpectedType>
-template <typename Iter>
-bool check_container_iterator(Iter iter) {
-    return std::is_same_v<std::iterator_traits<Iter::iterator>::iterator_category, ExpectedType>;
-}
-*/
-
-template <typename ExpectedType, typename Iter>
-bool check_container_iterator(Iter iter) {
-    typedef typename std::iterator_traits<Iter>::iterator_category Category;
-    return std::is_same_v<Category, ExpectedType>;
-}
-
-TEST(iterator_category, check_type) {
-    std::vector<int> v{1, 2};
-    //  In instantiation of 'bool check_container_iterator(Iter) [with ExpectedType = std::random_access_iterator_tag;
-    //  Iter = __gnu_cxx::__normal_iterator<int*, std::vector<int> >]':
-    EXPECT_TRUE(check_container_iterator<std::random_access_iterator_tag>(v.begin()));
-
-    // 1. vector
-    constexpr bool vector_is_random =
-      std::is_same_v<std::iterator_traits<std::vector<int>::iterator>::iterator_category,
-                     std::random_access_iterator_tag>;
-    EXPECT_TRUE(vector_is_random);
-
-    constexpr bool vector_is_bidirectional =
-      std::is_same_v<std::iterator_traits<std::vector<int>::iterator>::iterator_category,
-                     std::bidirectional_iterator_tag>;
-    EXPECT_FALSE(vector_is_bidirectional);
-
-    // 2. list
-    constexpr bool list_is_random = std::is_same_v<std::iterator_traits<std::list<int>::iterator>::iterator_category,
-                                                   std::random_access_iterator_tag>;
-    EXPECT_FALSE(list_is_random);
-
-    constexpr bool list_is_bidirectional =
-      std::is_same_v<std::iterator_traits<std::list<int>::iterator>::iterator_category,
-                     std::bidirectional_iterator_tag>;
-    EXPECT_TRUE(list_is_bidirectional);
-
-    // 3. raw array
-    int data[]{1, 2, 3, 4, 5};
-    // std::begin(data) error: 'decltype' evaluates to 'int*', which is not a class or enumeration type
-}
 
 TEST(iterator_category, my_reverse) {
     // int a[]{1, 2, 3, 4, 5};
